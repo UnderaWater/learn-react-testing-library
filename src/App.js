04d59bf01d935e12aac5a531e4389b26d5168b49
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-04-15 17:28:08 +0300

    add: added tests for async and other

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState('');
+
+  const handleClick = () => setToggle(prev => !prev);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setData({})
+    }, 300)
+  }, [])
+
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <button>Click</button>
-      <input type='text' placeholder='text...' />
+      <h2 data-testid='value-elem'>{value}</h2>
+      {data && <div>text div</div>}
+      {toggle === true && <div data-testid='toggle-elem'>toggle</div>}
+      <button data-testid='toggle-btn' onClick={handleClick}>Click</button>
+      <input onChange={(e) => setValue(e.target.value)} type='text' placeholder='text...' />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const helloWorldElem = screen.getByText(/hello world/i);
-  expect(linkElement).toBeInTheDocument();
+describe('test', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/text.../i)
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+
+  test('renders learn react link', () => {
+    render(<App />);
+    const isElem = screen.queryByText(/hello2/i);
+    expect(isElem).toBeNull();
+  });
+  
+  test('async test', async () => {
+    render(<App />);
+    const data = await screen.findByText(/text div/i);
+    expect(data).toBeInTheDocument();
+  })
+
+  test('event click', () => {
+    render(<App />);
+    const btn = screen.getByTestId('toggle-btn');
+    expect(screen.queryByTestId('toggle-elem')).toBeNull(); 
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-elem')).toBeNull(); 
+  })
+
+  test('event input', () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText(/text.../i);
+    expect(screen.queryByTestId('value-elem')).toContainHTML('');
+    // fireEvent.input(input, {
+    //   target: { value: 'qwerty' }
+    // });
+    userEvent.type(input, 'qwerty');
+    expect(screen.queryByTestId('value-elem')).toContainHTML('qwerty');
+  })
 });
